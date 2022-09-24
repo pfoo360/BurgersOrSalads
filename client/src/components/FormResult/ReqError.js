7fd589d68bfc6959a046a8cd5eac5ac52b043718
Phillip Foo <pfoo360@gmail.com>
2022-09-24 07:47:22 -0400

    removed border from ReqError.js

--- client/src/components/FormResult/ReqError.js
+++ client/src/components/FormResult/ReqError.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ReqError = ({ results }) => {
   return (
-    <h1 className="border-2 text-lg text-gray-700 m-2 w-auto flex justify-center items-center text-center max-w-[1024px] lg:mx-auto">
+    <h1 className="text-lg text-gray-700 m-2 w-auto flex justify-center items-center text-center max-w-[1024px] lg:mx-auto">
       Oops. Looks like something went wrong. 😖
     </h1>
   );
